43d950b4a73dbe6944022efe6b0fabf2f242a1bc
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-09-30 21:56:27 +0200

    Support integration tests

--- bin.js
+++ bin.js
@@ -40,7 +40,7 @@ const run = command(
       }
     }
     if (run.rest) {
-      for (const repo of run.rest) repos.push(repo)
+      for (const repo of run.rest) repos.push({ name: repo })
     }
 
     if (repos.length === 0) {
@@ -58,7 +58,7 @@ const run = command(
 
     let exit = 0
     for (const repo of repos) {
-      console.log(`Testing ${repo}`)
+      console.log(`Testing ${repo.name}`)
       await test(repo)
     }
 
